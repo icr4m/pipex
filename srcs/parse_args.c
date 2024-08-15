d38254e7234acd70191e6edf375c460869216e3e
Icham Jaber <ijaber@student.42.fr>
2024-08-15 23:01:22 +0200

    update

--- srcs/parse_args.c
+++ srcs/parse_args.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 03:32:19 by ijaber            #+#    #+#             */
-/*   Updated: 2024/08/14 04:56:26 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/08/15 22:29:41 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,9 @@ static char	*find_command(char **multiple_path, char *av, t_pipex *pipex)
 		command = ft_strjoin(temp, av);
 		free(temp);
 		if (access(command, X_OK) != -1)
+		{
 			return (command);
+		}
 		free(command);
 		i++;
 	}
@@ -40,11 +42,11 @@ void	parse_args(t_pipex *pipex)
 	size_t	i;
 
 	i = INDEX_START;
+	pipex->cmd_full = (char **)malloc(pipex->cmd_count * sizeof(char *));
 	while (i < pipex->cmd_count)
 	{
 		pipex->cmd_full[i] = find_command(pipex->cmd_paths,
 				pipex->args_paths[i][0], pipex);
-		ft_printf("test\n");
 		i++;
 	}
 }
