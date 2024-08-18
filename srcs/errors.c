37c518c4b69a17d190fedbe1d5d726031e66a2c0
Icham Jaber <ijaber@student.42.fr>
2024-08-18 19:37:38 +0200

    update

--- srcs/errors.c
+++ srcs/errors.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 03:55:11 by ijaber            #+#    #+#             */
-/*   Updated: 2024/08/14 03:58:54 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/08/18 19:12:01 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 void	pipex_error(char *error_message)
 {
 	ft_printf("Error\n");
-	ft_printf("%s\n", error_message);
+	perror(error_message);
 	exit(EXIT_FAILURE);
 }
 
 void	pipex_error_free(char *error_message, t_pipex *pipex)
 {
 	ft_printf("Error\n");
-	ft_printf("%s\n", error_message);
+	perror(error_message);
 	exit(EXIT_FAILURE);
 }
