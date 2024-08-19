73b6f002ffb4b017b5fa6025fd77d79738d1f7c6
Icham Jaber <ijaber@student.42.fr>
2024-08-19 07:07:09 +0200

    update

--- srcs/exec.c
+++ srcs/exec.c
@@ -6,23 +6,32 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 22:31:01 by ijaber            #+#    #+#             */
-/*   Updated: 2024/08/16 05:09:06 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/08/19 07:03:12 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void	child(void)
+{
+	int	fd;
+	fd = open_file()
+}
+
+void	parent(void)
+{
+}
+
 void	exec(t_pipex *pipex)
 {
-	pipe();
-	fork();
-	if (child)
-	{
-		dup2();
-		execve();
-	}
-	else
-	{
-		close();
-	}
+	int		pipefd[2];
+	pid_t	pid;
+
+	if (pipe(pipefd) == -1)
+		pipex_error("pipe error");
+	pid = fork();
+	if (pid == -1)
+		pipex_error("fork error");
+	else if (pid == 0)
+		child
 }
