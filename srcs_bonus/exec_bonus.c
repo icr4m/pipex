e5946fdfa4920e27e50ecf38ee4228730402e0bb
Icham Jaber <ijaber@student.42.fr>
2024-08-20 19:17:33 +0200

    update

--- srcs_bonus/exec_bonus.c
+++ srcs_bonus/exec_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 18:32:06 by ijaber            #+#    #+#             */
-/*   Updated: 2024/08/20 18:54:27 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/08/20 19:17:29 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,13 @@ void	create_here_doc(t_pipex *pipex, char **av, int *pipe_fd)
 	while (1)
 	{
 		read = get_next_line(0);
+		if (ft_strcmp(read, av[2]) == 0)
+		{
+			free(read);
+			exit(EXIT_SUCCESS);
+		}
+		ft_putstr_fd(read, pipe_fd[1]);
+		free(read);
 	}
 }
 
@@ -34,10 +41,16 @@ void	exec_here_doc(t_pipex *pipex, char **av)
 	if (pid == -1)
 		pipex_error_free("fork error", pipex);
 	else if (pid == 0)
-		create_here_doc(av, pipe_fd);
+		create_here_doc(pipex, av, pipe_fd);
+	else
+	{
+		close(pipe_fd[1]);
+		dup2(pipe_fd[0], 0);
+		wait(NULL);
+	}
 }
 
-void	exec_bonus(t_pipex *pipex, char **av)
+void	exec_bonus(t_pipex *pipex, char **av, int ac)
 {
 	int	index_ARGV;
 
@@ -49,4 +62,19 @@ void	exec_bonus(t_pipex *pipex, char **av)
 			pipex_error_free("open failed", pipex);
 		exec_here_doc(pipex, av);
 	}
+	else
+	{
+		index_ARGV = 2;
+		pipex->in_fd = open(av[1], O_RDONLY);
+		if (pipex->in_fd == -1)
+			pipex_error_free("open failed", pipex);
+		pipex->out_fd = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		if (pipex->out_fd == -1)
+			pipex_error_free("open failed", pipex);
+		dup2(pipex->in_fd, 0);
+	}
+	while (index_ARGV < ac - 2)
+		do_pipe(av[index_ARGV++], pipex);
+	dup2(pipex->out_fd, 1);
+	exec(av[ac - 2], pipex);
 }
