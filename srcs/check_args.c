524fa726e044da16c16f7b08e5591fe856605050
Icham Jaber <ijaber@student.42.fr>
2024-08-21 09:04:24 +0200

    🧙

--- srcs/check_args.c
+++ srcs/check_args.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 03:52:29 by ijaber            #+#    #+#             */
-/*   Updated: 2024/08/21 07:54:52 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/08/21 09:04:11 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,10 @@ void	check_args(int ac, char **av, t_pipex *pipex)
 		pipex_error("Usage : ./pipex file1 cmd1 cmd2 file2");
 	if (access(av[1], R_OK) == -1)
 		pipex_error("Permission denined on file1");
+	if (access(av[4], F_OK) != -1)
+	{
+		if (access(av[4], O_WRONLY | O_CREAT | O_TRUNC) == -1)
+			pipex_error("Permission denied on file2");
+	}
 	pipex->here_doc = 0;
 }
--- srcs/init_pipex.c
+++ srcs/init_pipex.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 00:30:48 by ijaber            #+#    #+#             */
-/*   Updated: 2024/08/21 07:55:07 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/08/21 08:31:23 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 void	init_pipex(t_pipex *pipex, int ac)
 {
 	if (pipex->here_doc == 0)
+		pipex->cmd_count = (size_t)ac - 3;
+	if (pipex->here_doc == 1)
 		pipex->cmd_count = (size_t)ac - 4;
 	pipex->cmd_full = NULL;
 	pipex->cmd_paths = NULL;
